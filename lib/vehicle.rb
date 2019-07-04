d6211365276c1b5ec6dc4ddea15d65aa7fe6dd01
Tam Vu <vutam_a@hotmail.com>
2019-07-04 15:54:50 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,12 @@
 class Vehicle
-
+  
+  attr _accessor :wheel_size, :wheel_number
+  
+  def inistialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end 
+  
+  def go 
+    
 end
